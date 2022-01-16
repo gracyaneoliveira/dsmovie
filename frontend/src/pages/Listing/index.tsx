0087630fc67c0a81ae5ed0139b70a34e739dc339
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2022-01-16 19:08:48 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,15 +1,22 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "utils/types/movie";
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
 
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
-  .then(response => {
-    console.log(response.data);
-  });
-
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response => {
+      const data = response.data as MoviePage
+      console.log(data);
+      
+      setPageNumber(data.number)
+    });
+  },[]);
 
   return (
     <>
